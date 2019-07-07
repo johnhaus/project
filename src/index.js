24e6249ceca74f31ff18a1ef8b53f13b21ff0c54
johnhaus <john.haus@outlook.com>
2019-07-07 16:22:36 -0300

    added api fetching

--- src/index.js
+++ src/index.js
@@ -7,20 +7,50 @@ let bookList = [
   {"title": "Cat's Cradle", "author": "Kurt Vonnegut", pages:304}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
   return (
     <section>
       <h2>{title}</h2>
       <p>By: {author}</p>
       <p>Pages: {pages} pages</p>
+      <p>Free Bookmark Today: {freeBookmark ? 'yes' : 'no'}</p>
     </section>
   )
 }
 
-//
+// different way of writing function- without curly braces
+const Hiring = () =>
+  <div>
+    <p>The library is hiring. See our website for more.</p>
+  </div>
+
+const NotHiring = () =>
+  <div>
+    <p>The library is not hiring now.</p>
+  </div>
+
+
 class Library extends React.Component {
   // Allows removal of constructor
-  state = { open: false }
+  state = {
+    open: true,
+    freeBookmark: true,
+    hiring: true,
+    data: [],
+    loading: false
+   }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    // fetch data from rest api
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+    .then(data => data.json())
+    .then(data => this.setState({data, loading: false}))
+  }
+
+  componentDidUpdate() {
+    console.log("The component has updated")
+  }
 
   toggleOpenClosed = () => {
     // Using a callback function here
@@ -38,6 +68,21 @@ class Library extends React.Component {
     const { books } = this.props
     return (
       <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        {this.state.loading
+          ? "loading..."
+          : <div>
+            {this.state.data.map(product => {
+              return (
+                <div key={product.id}>
+                  <h3>Library product of the week</h3>
+                  <h4>{product.name}</h4>
+                  <img alt={product.name} src={product.image} height={100}/>
+                </div>
+              )
+            })}
+          </div>
+        }
         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map(
@@ -47,7 +92,8 @@ class Library extends React.Component {
               key={i}
               title={book.title}
               author={book.author}
-              pages={book.pages}/>
+              pages={book.pages}
+              freeBookmark={this.state.freeBookmark}/>
           )}
       </div>
     )
